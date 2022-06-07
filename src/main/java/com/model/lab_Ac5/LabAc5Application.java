96cf261705f90c0d8718ddbaf58892bdc9171106
OlympiaBiddle <osb399@gmail.com>
2022-06-07 15:27:33 -0400

    first commit

---
+++ src/main/java/com/model/lab_Ac5/LabAc5Application.java
@@ -0,0 +1,13 @@
+package com.model.lab_Ac5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAc5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAc5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/model/lab_Ac5/LabAc5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.model.lab_Ac5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabAc5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

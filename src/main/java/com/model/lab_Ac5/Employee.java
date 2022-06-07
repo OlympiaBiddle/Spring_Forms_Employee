d12a7b92a01e7d04ad41346e2cf1048c67892c33
OlympiaBiddle <osb399@gmail.com>
2022-06-07 15:53:39 -0400

    added Employee and EmployeeController Class. Added a html form that will allow the user to input an id number, name, and phone number. The employeeview html will return the data the user submitted

---
+++ src/main/java/com/model/lab_Ac5/Employee.java
@@ -0,0 +1,46 @@
+package com.model.lab_Ac5;
+
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Employee {
+   private long id;
+   private String name;
+   private String contactNumber;
+
+   public String getContactNumber() {
+       return contactNumber;
+   }
+
+   public void setContactNumber(String contactNumber) {
+       this.contactNumber = contactNumber;
+   }
+
+   public Employee(long id, String name) {
+       this.id = id;
+       this.name = name;
+   }
+
+   public long getId() {
+       return id;
+   }
+
+   public void setId(long id) {
+       this.id = id;
+   }
+
+   public String getName() {
+       return name;
+   }
+
+   public void setName(String name) {
+       this.name = name;
+   }
+
+   // standard getters and setters removed
+}
+
